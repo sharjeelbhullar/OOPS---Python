b83ab3baf616cf9b0e3630c088ff81657c7c6c7a
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-10-16 23:01:57 +0500

    Write some code for oop concepts to understand

---
+++ oop-1.py
@@ -0,0 +1,35 @@
+class Data():
+    def __init__(self):
+        print("hello checking concepts of instructor.")
+
+
+    def __init__(self, name, roll_no, email, batch):
+        self.name = name
+        self.roll_no =  roll_no
+        self.email = email
+        self.batch = batch
+    
+s1 = Data()
+
+
+
+# student1 = Data("Sharjeel", 1054, "sharjeelbhullar@gmail.com", 2019)
+# student2 = Data("Ahmad", 1021, "xsharjeelahmad@gmail.com", 2019)
+# student3 = Data("Ali", 1035, "sharjeelbhullar68@gmail.com", 2023)
+
+# s1_list = [student1.name, student1.roll_no, student1.email, student1.batch]
+# s2_list = [student2.name, student2.roll_no, student2.email, student2.batch]
+# s3_list = [student3.name, student3.roll_no, student3.email, student3.batch]
+
+# def evaluating(name):
+#         for name in s1_list:
+#             print(student1.roll_no)
+#             break
+#         else:
+#             print("Not existed")
+
+# print(s1_list)
+# print(s2_list)
+# print(s3_list)
+
+# evaluating("Sharjeel")
