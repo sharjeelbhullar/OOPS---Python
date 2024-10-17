e5aabd7c514b8c16ef4fbd1d2b62ab78db7a901e
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-10-17 11:10:11 +0500

    Encapsulation code

---
+++ encapsulation.py
@@ -0,0 +1,18 @@
+class Account:
+    def __init__(self, bal, acc):
+        self.balance = bal
+        self.account_no = acc
+
+    def debit(self, amount):
+        self.balance -= amount
+        print("Rs.", amount, "was debited")
+        print("Total balance = ", self.balance)
+
+    def credit(self, amount):
+        self.balance += amount
+        print("Rs.", amount, "was credited.")
+        print("Total balance = ", self.balance)
+
+user1 = Account(98000, 432)
+user1.credit(3000)
+user1.debit(2000)
