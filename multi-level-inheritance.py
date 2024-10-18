952823e1f9855f57922e0db929d507e742d6c286
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-10-18 12:31:44 +0500

    Multi-level-Inheritance

--- multi-level-inheritance.py
+++ multi-level-inheritance.py
@@ -14,18 +14,23 @@ class ToyotaCar(Car):
     def __coloravailable(self, colors):
         self.colors = colors
 
+    def getcolors(self):
+        print(self.colors)
+
 class Prius(ToyotaCar):
     def __init__(self, model, year, brand, colors):
         self.model = model
         self.year = year
-        self.brand = brand
-        self.colors = colors
+        super().__init__(brand)
+        self._ToyotaCar__coloravailable(colors)  
 
-user1 = Prius("A3", 2023, "Toyota", ["red", "blue", "green"])
-# user1.start()
-# print(f"Brand = {user1.brand}")
-# print(user1.colors)
-# print(user1.model)
-# print(user1.year)
+user1 = Prius("A3", 2023, "Toyota", ["white", "black", "silver"])
+user1.start()
+user1.stop()
+print(user1.brand)
+print(user1.model)
+print(user1.year)
+user1.getcolors()
+
 
 
